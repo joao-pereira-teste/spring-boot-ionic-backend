7a48c5c49f1b4a3a1981f147e095f76e98774d20
João Pereira <joao.2009.1326@hotmail.com>
2020-06-05 21:38:09 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/estudandoemcasa/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/estudandoemcasa/cursomc/domain/ItemPedidoPK.java
@@ -7,7 +7,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 
-//@JsonIgnore
+
 @Embeddable
 public class ItemPedidoPK implements Serializable {
 	private static final long serialVersionUID = 1L;
